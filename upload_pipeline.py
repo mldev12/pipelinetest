d9df6f2f449f2d00b086b4f47d017f71bbcc1494
mldev12 <163518876+mldev12@users.noreply.github.com>
2024-04-22 14:41:10 +0800

    Status

--- upload_pipeline.py
+++ upload_pipeline.py
@@ -126,8 +126,9 @@ run_id = run.run_id
 # Wait for the pipeline run to complete
 try:
     final_status = client.wait_for_run_completion(run_id, 36000)
-    run = client.get_run(run_id)
-    print(f"Run completed with status: {run.run.status}")
+    get_run_response = self._run_api.get_run(run_id=run_id)
+    status = get_run_response.run.status
+    print(f"Run completed with status: {status}")
 except TimeoutError as e:
     print(str(e))
 
