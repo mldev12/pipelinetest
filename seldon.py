754003a3dd3379e1c4d76d8e5148cd08244fcc07
mldev12 <163518876+mldev12@users.noreply.github.com>
2024-05-02 15:38:04 +0800

    Update seldon.py

--- seldon.py
+++ seldon.py
@@ -2,65 +2,59 @@ import yaml
 
 def generate_kubernetes_yaml(model_uri, run_id):
     kubernetes_resources = {
-        "apiVersion": "v1",
-        "kind": "List",
-        "items": [
-            {
-                "apiVersion": "machinelearning.seldon.io/v1alpha2",
-                "kind": "SeldonDeployment",
-                "metadata": {
-                    "name": f"sdp-{run_id}",
-                    "namespace": "seldon-system"
+        "apiVersion": "machinelearning.seldon.io/v1alpha2",
+        "kind": "SeldonDeployment",
+        "metadata": {
+            "name": f"sdp-{run_id}",
+            "namespace": "seldon-system"
+        },
+        "spec": {
+            "name": "income-model",
+            "predictors": [{
+                "graph": {
+                    "children": [],
+                    "implementation": "MLFLOW_SERVER",
+                    "modelUri": model_uri,
+                    "envSecretRefName": "seldon-init-container-secret",
+                    "name": "mlflow-model"
                 },
-                "spec": {
-                    "name": "income-model",
-                    "predictors": [{
-                        "graph": {
-                            "children": [],
-                            "implementation": "MLFLOW_SERVER",
-                            "modelUri": model_uri.replace(""", "")),
-                            "envSecretRefName": "seldon-init-container-secret",
-                            "name": "mlflow-model"
-                        },
-                        "name": "default",
-                        "replicas": 1,
-                        "componentSpecs": [{
-                            "spec": {
-                                "containers": [{
-                                    "name": "classifier",
-                                    "image": "seldonio/mlflowserver:latest",
-                                    "envFrom": [{
-                                        "secretRef": {
-                                            "name": "seldon-init-container-secret"
-                                        }
-                                    }],
-                                    "livenessProbe": {
-                                        "failureThreshold": 3,
-                                        "initialDelaySeconds": 5000,
-                                        "periodSeconds": 5,
-                                        "successThreshold": 1,
-                                        "tcpSocket": {
-                                            "port": "http"
-                                        },
-                                        "timeoutSeconds": 1
-                                    },
-                                    "readinessProbe": {
-                                        "failureThreshold": 3,
-                                        "initialDelaySeconds": 5000,
-                                        "periodSeconds": 5,
-                                        "successThreshold": 1,
-                                        "tcpSocket": {
-                                            "port": "http"
-                                        },
-                                        "timeoutSeconds": 1
-                                    }
-                                }]
+                "name": "default",
+                "replicas": 1,
+                "componentSpecs": [{
+                    "spec": {
+                        "containers": [{
+                            "name": "classifier",
+                            "image": "seldonio/mlflowserver:latest",
+                            "envFrom": [{
+                                "secretRef": {
+                                    "name": "seldon-init-container-secret"
+                                }
+                            }],
+                            "livenessProbe": {
+                                "failureThreshold": 3,
+                                "initialDelaySeconds": 5000,
+                                "periodSeconds": 5,
+                                "successThreshold": 1,
+                                "tcpSocket": {
+                                    "port": "http"
+                                },
+                                "timeoutSeconds": 1
+                            },
+                            "readinessProbe": {
+                                "failureThreshold": 3,
+                                "initialDelaySeconds": 5000,
+                                "periodSeconds": 5,
+                                "successThreshold": 1,
+                                "tcpSocket": {
+                                    "port": "http"
+                                },
+                                "timeoutSeconds": 1
                             }
                         }]
-                    }]
-                }
-            }
-        ]
+                    }
+                }]
+            }]
+        }
     }
 
     with open('kubernetes_resources.yaml', 'w') as f:
