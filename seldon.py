3fe00314bc9cf08beeac55b3e9b33c27c578ba78
mldev12 <163518876+mldev12@users.noreply.github.com>
2024-05-02 12:38:31 +0800

    Update seldon.py

--- seldon.py
+++ seldon.py
@@ -18,7 +18,7 @@ def generate_kubernetes_yaml(model_uri, run_id):
                         "graph": {
                             "children": [],
                             "implementation": "MLFLOW_SERVER",
-                            "model_uri": model_uri,
+                            "modelUri": model_uri,
                             "envSecretRefName": "seldon-init-container-secret",
                             "name": "mlflow-model"
                         },
